d968727364b26dfc26cd030f97c6f067433fa051
Eric Berglund <ericberglund117@gmail.com>
2020-11-07 11:37:17 -0700

    Allow user to have multiple random activities

---
+++ src/activityArea/ActivityArea.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ActivityArea.css'
+
+const ActivityArea = (props) => {
+  const { activity } = props
+  return (
+    activity.map(act => {
+      return (
+      <section className="activity-card">
+        <h2>Activity</h2>
+        <h3>{act.activity}</h3>
+        <h3>Type: {act.type}</h3>
+        <h3>Participants: {act.participants}</h3>
+        <h3>Price: {act.price}</h3>
+      </section>
+      )
+    })
+  )
+}
+
+export default ActivityArea;
