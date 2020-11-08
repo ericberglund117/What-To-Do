b65897b3a3d5edff9c6ec05d2459633297f644e8
Eric Berglund <ericberglund117@gmail.com>
2020-11-08 16:21:47 -0700

    Add styling to form section

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
       <section className='background' title='background'>
         <h1>What To Do?</h1>
         <img alt='michael-logo' src={MichaelScott} />
-        <h2>Open to possibilities</h2>
       </section>
     </section>
   )
