b56a88f01768ce50728a14dcf4f5d6a7c61b65e0
Eric Berglund <ericberglund117@gmail.com>
2020-11-09 15:57:14 -0700

    Test to allow user to search for a random activity via number of participants

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -16,7 +16,17 @@ const expectedActivity = [{
   link: "",
   key: "6081071",
   accessibility: 0.2
-}];
+  }];
+
+const expectedParticipantsActivity = [{
+    activity: "Play Settlers of Catan",
+    type: "social",
+    participants: 3,
+    price: 0.05,
+    link: "",
+    key: "6081071",
+    accessibility: 0.2
+  }];
 
 describe('App', () => {
   it('should be able to display a random activity when a user clicks the Search Activities button with no inputs filled out', async () => {
@@ -45,6 +55,45 @@ describe('App', () => {
       </MemoryRouter>
     )
     const activityName = screen.getByText("Text a friend you haven't talked to in a long time");
+    expect(activityName).toBeInTheDocument();
+  });
+
+  it('should be able to display a random activity when a user clicks the Search Activities button with participants input filled out', async () => {
+    getActivityParticipants.mockResolvedValueOnce(expectedParticipantsActivity)
+    const mockRandomActivity = jest.fn()
+
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const activityArea = screen.getByText('Activities List')
+    expect(activityArea).toBeInTheDocument();
+
+    const participantsInput = screen.getByRole('combobox')
+    expect(participantsInput).toBeInTheDocument();
+
+    const form = screen.getByTestId('activity-search');
+    expect(form).toHaveFormValues({ participants: '0' });
+
+    const participants = screen.getByLabelText('Participants')
+    userEvent.selectOptions(participants, '3');
+    expect(form).toHaveFormValues({ participants: '3' });
+
+    const submitButton = screen.getByRole('button', { name: /search activities/i })
+    userEvent.click(submitButton)
+    expect(getActivityParticipants).toHaveBeenCalledTimes(1);
+
+    const receivedActivity = await waitFor(() => expectedParticipantsActivity)
+    expect(receivedActivity).toHaveLength(1)
+
+    render(
+      <MemoryRouter>
+        <ActivityArea activities={expectedParticipantsActivity} />
+      </MemoryRouter>
+    )
+    const activityName = screen.getByText("Play Settlers of Catan");
     expect(activityName).toBeInTheDocument();
   })
 })
