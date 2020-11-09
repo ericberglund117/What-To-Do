164877362b03b60f087c58d751346c0889733b84
Eric Berglund <ericberglund117@gmail.com>
2020-11-09 12:58:24 -0700

    Update styling on activity card

--- src/activityCard/ActivityCard.js
+++ src/activityCard/ActivityCard.js
@@ -6,11 +6,11 @@ const ActivityCard = (props) => {
     if(props.cardKey === activity.key) {
       return (
         <section className="activity-card" key={index}>
-          <h2>Activity</h2>
-          <h3>{activity.activity}</h3>
+          <h2 className="act">Activity</h2>
+          <h3 className="act-name">{activity.activity}</h3>
           <h3>Type: {activity.type}</h3>
           <h3>Participants: {activity.participants}</h3>
-          <h3>Price: {activity.price}</h3>
+          <h3>Price:$ {activity.price}</h3>
           <h3>Key: {activity.key}</h3>
         </section>
       )
